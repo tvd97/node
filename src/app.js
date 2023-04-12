d30c021031eead0b1a37aef075e4cd93fb6d497b
dantv <dantv@rikkeisoft.com>
2023-04-12 13:37:12 +0700

    ...

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
-const path = require('path');
+const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const morgan = require("morgan");
+const route = require("./routes/index");
+
+const path = require("path");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 4000;
-const route = require('./routes/index');
-const mongoClient = require("mongodb").MongoClient;
-const url =
-  "mongodb+srv://d1715:d13517f@dantv.9e5ud4b.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(
+  "mongodb://root:rootpassword@10.23.55.245:27017/"
+);
 
-mongoClient.connect(url, function (err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
 
-//http logger
-app.use(morgan('tiny'));
+// Use connect method to connect to the Server
+client.connect().then(() => console.log("connected"));
+
+
+app.use(morgan("tiny"));
 
 // midleware express < 4.16 intall body_parser
 //xml http, fetch
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '.\\resources\\views'));
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, '.','resources','views'));
 
 // path file
-app.use(express.static(path.join(__dirname, '.\\resouces')));
+app.use(express.static(path.join(__dirname, '.','resouces')));
 
 // init router
 route(app);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} ${__dirname}`);
+  console.log(`Server app listening on port ${port} ${__dirname}`);
 });
--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const route = express.Router();
 
 const controller = require('../app/controllers/HomeController');
-route.use('/', controller.index);
+route.get('/', controller.index);
 
 module.exports = route;
